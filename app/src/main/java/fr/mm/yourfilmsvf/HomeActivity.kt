95b92b27668f7f62c4b1dd63a94a47843c78b76b
GuillaumeBdrd <guillaume.bodard@epfedu.fr>
2023-06-04 22:37:44 +0200

    début tests api

--- app/src/main/java/fr/mm/yourfilmsvf/HomeActivity.kt
+++ app/src/main/java/fr/mm/yourfilmsvf/HomeActivity.kt
@@ -3,17 +3,16 @@ package fr.mm.yourfilmsvf
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val addButton = findViewById<Button>(R.id.home_add_button)
-
-        addButton.setOnClickListener{
-            Intent(this, AddClientActivity::class.java)
-        }
+        val Film = findViewById<TextView>(R.id.Film)
+        Film.text = "ok"
 
 
 
