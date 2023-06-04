76ef2ec71baaa24eb24b44b63e7c5344049e5430
GuillaumeBdrd <guillaume.bodard@epfedu.fr>
2023-06-04 21:49:25 +0200

    Design home + Film

---
+++ app/src/main/java/fr/mm/yourfilmsvf/HomeActivity.kt
@@ -0,0 +1,21 @@
+package fr.mm.yourfilmsvf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val addButton = findViewById<Button>(R.id.home_add_button)
+
+        addButton.setOnClickListener{
+            Intent(this, AddClientActivity::class.java)
+        }
+
+
+
+    }
+}
